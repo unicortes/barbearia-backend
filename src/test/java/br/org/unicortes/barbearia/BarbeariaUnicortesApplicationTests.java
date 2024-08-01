5f2d688183852727f7805539e54638614e79f319
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-31 22:17:04 -0300

    Criando a estrutura do projeto

---
+++ src/test/java/br/org/unicortes/barbearia/BarbeariaUnicortesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.unicortes.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaUnicortesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
