f06b1ffb8384941f2e1a228af46feb304ad646cb
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-08-19 15:00:35 -0300

    Correção nos imports de PromocaoServiceTest

--- src/test/java/br/org/unicortes/barbearia/services/PromocaoServiceTest.java
+++ src/test/java/br/org/unicortes/barbearia/services/PromocaoServiceTest.java
@@ -6,6 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.org.unicortes.barbearia.models.Promocao;
+import br.org.unicortes.barbearia.repositories.PromocaoRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 import java.util.Arrays;
 import java.util.Optional;
