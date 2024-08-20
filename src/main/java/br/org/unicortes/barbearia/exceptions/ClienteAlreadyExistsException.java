8376cca840133b45843c35b115723dd2e620fb1d
Matheusxr77 <suehtamxr77@gmail.com>
2024-08-20 01:07:18 -0300

    update: nome da exception

--- src/main/java/br/org/unicortes/barbearia/exceptions/ClienteAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.org.unicortes.barbearia.exceptions;
-
-public class ClienteAlreadyExistsException extends RuntimeException {
-    public ClienteAlreadyExistsException(String name) {
-        super("O Cliente '" + name + "' já existe.");
-    }
-}
