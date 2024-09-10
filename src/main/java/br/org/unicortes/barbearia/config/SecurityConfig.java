8ebba71925b13af97196090ba65b969e5a7aed4f
Mayara <mayeufraferreira@gmail.com>
2024-09-09 22:01:19 -0300

    Refactoring - Change Function Declaration

--- src/main/java/br/org/unicortes/barbearia/config/SecurityConfig.java
+++ src/main/java/br/org/unicortes/barbearia/config/SecurityConfig.java
@@ -4,15 +4,13 @@ import br.org.unicortes.barbearia.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -49,6 +47,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/loytalty-cars/**",
                                 "/api/promocoes/**",
+                                "/api/barber/**",
+                                "/api/servicos/**",
                                 "/api/stocks/**",
                                 "/api/available-times/**",
                                 "/api/barber/**",
@@ -56,27 +56,15 @@ public class SecurityConfig {
                                 "/api/sales/**",
                                 "/api/servicos/**",
                                 "/api/appointments/**",
-                                "/api/products/**"
-                        ).hasRole("ADMIN")
-                        .requestMatchers(
-                                "/api/available-times/**",
-                                "/api/loytalty-cars/**",
-                                "/api/sales/**",
-                                "/api/servicos",
-                                "/api/stocks/**",
-                                "/api/appointments/**",
-                                "/api/products/**"
-                        ).hasRole("BARBER")
-                        .requestMatchers(
+                                "/api/products/**",
                                 "/api/available-times/",
                                 "/api/appointments",
                                 "/api/appointments/available",
-                                "/api/barber/**",
-                                "/api/servicos/**"
-                        ).hasRole("CLIENT")
-                        .requestMatchers("/home").permitAll()
+                                "/home"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
+
                 .formLogin(form -> form
                         .loginProcessingUrl("/barbeariaUnicortes/login")
                         .successHandler(customAuthenticationSuccessHandler)
@@ -93,21 +81,10 @@ public class SecurityConfig {
         return http.build();
     }
 
-
     @Bean
     public UserDetailsService userDetailsService() {
-
-        /*UserDetails admin = User.builder()
-                .username("admin")
-                .password("$2a$12$AvcVZi9.Ak.Li7iqu9/d7OdnyLouk6wO2tK4e1nB0x5stcmW9foZK")
-                .roles("ADMIN", "CLIENT", "BARBER")
-                .build();
-
-        System.out.println(admin);*/
-
         return authService;
     }
-
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -121,5 +98,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
 }
