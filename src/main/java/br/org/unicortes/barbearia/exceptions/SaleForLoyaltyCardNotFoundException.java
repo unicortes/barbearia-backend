e6d86dad796d5268af05a360e509b583c7dd49e7
Matheusxr77 <suehtamxr77@gmail.com>
2024-09-10 00:27:08 -0300

    Refactoring - Remove Dead Code Sale

--- src/main/java/br/org/unicortes/barbearia/exceptions/SaleForLoyaltyCardNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.org.unicortes.barbearia.exceptions;
-
-public class SaleForLoyaltyCardNotFoundException extends RuntimeException {
-    public SaleForLoyaltyCardNotFoundException (Long id) {
-        super("Venda por cartão fidelidade com o id '" + id + "' não encontrado.");
-    }
-}
--- src/main/java/br/org/unicortes/barbearia/exceptions/SaleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.org.unicortes.barbearia.exceptions;
-
-public class SaleNotFoundException extends RuntimeException {
-    public SaleNotFoundException (Long id) {
-        super("Venda com o id '" + id + "' não encontrada.");
-    }
-}
